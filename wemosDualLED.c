4dc676919b425660197cd195232d67e9de783b5d
LvndnB <150924843+LvndnB@users.noreply.github.com>
2025-04-03 13:55:10 +0200

    Update wemosDualLED.c naar PoC deel 1 ready

--- wemosDualLED.c
+++ wemosDualLED.c
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 
-const char* ssid = "SSID";
-const char* password = "Password";
+const char* ssid = "Revalidatie";
+const char* password = "Revalidatie";
 const int serverPort = 12345;
 const int roodledPin = D8;
 const int groenledPin = D7;
@@ -22,7 +22,7 @@ void setup() {
 
 // Functie voor besturen LED
 void bestuurLED(int pin, int status) {
-  digitalWrite(pin, status);
+  digitalWrite(pin, !status);
 }
 
 void loop() {
@@ -33,13 +33,12 @@ void loop() {
     input.trim();
     if (sscanf(input.c_str(), "%49[^ = ] = %d", key, &value) == 2) {  // String parsing voor key = value
       if (value == 1 || value == 0) {                                 // Aansturing van besturenLED functie
-        if (strcmp(key, "DualLEDRood") == 0) {
+        if (strcmp(key, "DUALLEDROOD") == 0) {
           bestuurLED(roodledPin, value);
         }
-        if (strcmp(key, "DualLEDGroen") == 0) {
+        if (strcmp(key, "DUALLEDGROEN") == 0) {
           bestuurLED(groenledPin, value);
         }
-        
       }
     }
   }
