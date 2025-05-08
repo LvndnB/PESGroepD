a6979ffc1775e41e380c3dafc98dbcd5b29e5eeb
Aron <aron@dingweb.nl>
2025-05-08 14:48:36 +0200

    Fist content commit

---
+++ Debug/Core/Src/sensirion_i2c_hal.d
@@ -0,0 +1,9 @@
+Core/Src/sensirion_i2c_hal.o: ../Core/Src/sensirion_i2c_hal.c \
+ ../Core/Inc/sensirion_i2c_hal.h ../Core/Inc/sensirion_config.h \
+ ../Core/Inc/sensirion_common.h ../Core/Inc/sensirion_arch_config.h \
+ ../Core/Inc/sensirion_config.h
+../Core/Inc/sensirion_i2c_hal.h:
+../Core/Inc/sensirion_config.h:
+../Core/Inc/sensirion_common.h:
+../Core/Inc/sensirion_arch_config.h:
+../Core/Inc/sensirion_config.h:
---
+++ Debug/Core/Src/sgp30.d
@@ -0,0 +1,17 @@
+Core/Src/sgp30.o: ../Core/Src/sgp30.c ../Core/Inc/sgp30.h \
+ ../Core/Inc/sensirion_arch_config.h ../Core/Inc/sensirion_common.h \
+ ../Core/Inc/sensirion_i2c.h ../Core/Inc/sensirion_config.h \
+ ../Core/Inc/sensirion_arch_config.h ../Core/Inc/sensirion_common.h \
+ ../Core/Inc/sensirion_i2c.h ../Core/Inc/sgp_git_version.h \
+ ../Core/Inc/sensirion_config.h ../Core/Inc/sensirion_i2c_hal.h
+../Core/Inc/sgp30.h:
+../Core/Inc/sensirion_arch_config.h:
+../Core/Inc/sensirion_common.h:
+../Core/Inc/sensirion_i2c.h:
+../Core/Inc/sensirion_config.h:
+../Core/Inc/sensirion_arch_config.h:
+../Core/Inc/sensirion_common.h:
+../Core/Inc/sensirion_i2c.h:
+../Core/Inc/sgp_git_version.h:
+../Core/Inc/sensirion_config.h:
+../Core/Inc/sensirion_i2c_hal.h:
