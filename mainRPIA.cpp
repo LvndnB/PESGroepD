bea833572e399f2fb678db4ae8b48aba37556715
LvndnB <150924843+LvndnB@users.noreply.github.com>
2025-05-13 14:37:34 +0200

    Update mainRPIA.cpp

--- mainRPIA.cpp
+++ mainRPIA.cpp
@@ -1,13 +1,13 @@
 #include "SocketServer.h"
-#include "WemosDualLED.h"
+#include "WemosDLEDenBuzzer.h"
 #include "WemosEncoder.h"
 
 #define PORT 12345
-#define WEMOSDUALLED_IP "192.168.0.106"
+#define WEMOSDLEDENBUZZER_IP "192.168.0.106"
 #define WEMOSENCODER_IP "192.168.0.100"
 
 int main() {
-    WemosDualLED dualled(WEMOSDUALLED_IP, PORT);
+    WemosDLEDenBuzzer dualled(WEMOSDLEDENBUZZER_IP, PORT);
     WemosEncoder encoder(WEMOSENCODER_IP, PORT, &dualled);
 
     SocketServer server(PORT, &encoder);
