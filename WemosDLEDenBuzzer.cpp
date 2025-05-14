e8791b53975b26ddeae95f33c419451c711064e2
LvndnB <150924843+LvndnB@users.noreply.github.com>
2025-05-14 12:09:55 +0200

    Update WemosDLEDenBuzzer.cpp

--- WemosDLEDenBuzzer.cpp
+++ WemosDLEDenBuzzer.cpp
@@ -5,13 +5,10 @@ WemosDLEDenBuzzer::WemosDLEDenBuzzer(const std::string& ip, int port) : Wemos(ip
 
 void WemosDLEDenBuzzer::aan(const std::string& kleur) {
     makeSendMessage("DUALLED" + kleur, "1");
-    std::cout << "Verzonden naar WemosLED: DUALLEDGROEN=1" << "\n";
 }
 
 void WemosDLEDenBuzzer::uit(const std::string& kleur) {
     makeSendMessage("DUALLED" + kleur, "0");
-    std::cout << "Verzonden naar WemosLED: DUALLEDGROEN=0" << "\n";
-
 }
 
 void WemosDLEDenBuzzer::buzzer(int status) {
