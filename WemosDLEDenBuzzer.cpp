78f6d97744815e3fa190e97fb05f892065e8ffa8
LvndnB <150924843+LvndnB@users.noreply.github.com>
2025-05-22 10:07:49 +0200

    Voeg logica toe voor juiste momenten aan en uit leds vanuit wemos

--- WemosDLEDenBuzzer.cpp
+++ WemosDLEDenBuzzer.cpp
@@ -1,16 +1,43 @@
 #include "WemosDLEDenBuzzer.h"
 #include <iostream>
+#include <cstring>
 
 WemosDLEDenBuzzer::WemosDLEDenBuzzer(const std::string& ip, int port) : Wemos(ip, port) {}
 
 void WemosDLEDenBuzzer::aan(const std::string& kleur) {
-    makeSendMessage("DUALLED" + kleur, "1");
+    if (kleur == "GROEN" && !ledroodstatus) {
+        ledgroenstatus = true;
+    } else if (kleur == "ROOD") {
+        ledroodstatus = true;
+        ledgroenstatus = false;
+    }
+
+    makeSendMessage("DUALLEDGROEN", std::to_string(ledgroenstatus));
+    makeSendMessage("DUALLEDROOD", std::to_string(ledroodstatus));
 }
 
 void WemosDLEDenBuzzer::uit(const std::string& kleur) {
-    makeSendMessage("DUALLED" + kleur, "0");
+    
+
+    if (kleur == "GROEN" && ledgroenstatus) {
+        ledgroenstatus = false;
+    } else if (kleur == "ROOD" && ledgroenstatus) {
+        ledroodstatus = false;
+    }
+
+    makeSendMessage("DUALLEDGROEN", std::to_string(ledgroenstatus));
+    makeSendMessage("DUALLEDROOD", std::to_string(ledroodstatus));
 }
 
 void WemosDLEDenBuzzer::buzzer(int status) {
     makeSendMessage("BUZZER", std::to_string(status));
+}
+
+bool WemosDLEDenBuzzer::isAan(const std::string& kleur) {
+    if (kleur == "GROEN") {
+        return ledgroenstatus;
+    } else if (kleur == "ROOD") {
+        return ledroodstatus;
+    }
+
 }
