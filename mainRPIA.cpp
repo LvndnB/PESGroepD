06c00e1053b1273fed8795cc4521e67d2016a8f9
LvndnB <150924843+LvndnB@users.noreply.github.com>
2025-05-22 10:12:10 +0200

    Logging toegevoegd

--- mainRPIA.cpp
+++ mainRPIA.cpp
@@ -2,6 +2,7 @@
 #include "WemosDLEDenBuzzer.h"
 #include "WemosEncoder.h"
 #include "WemosPulseOxy.h"
+#include "Logger.h"
 
 #define PORT 12345
 #define WEMOSDLEDENBUZZER_IP "192.168.0.106"
@@ -9,12 +10,19 @@
 #define WEMOSPULSEOXY_IP "192.168.0.107"
 
 int main() {
+    Logger encoderLog("encoderLog.log");
+    Logger pulseoxyLog("pulseoxyLog.log");
     WemosDLEDenBuzzer dualled(WEMOSDLEDENBUZZER_IP, PORT);
     WemosDLEDenBuzzer buzzer(WEMOSDLEDENBUZZER_IP, PORT);
-    WemosEncoder encoder(WEMOSENCODER_IP, PORT, &dualled);
-    WemosPulseOxy pulseoxy(WEMOSPULSEOXY_IP, PORT, &buzzer, &dualled);
+    WemosEncoder encoder(WEMOSENCODER_IP, PORT, &dualled, &encoderLog);
+    WemosPulseOxy pulseoxy(WEMOSPULSEOXY_IP, PORT, &buzzer, &dualled, &pulseoxyLog);
+    
 
     SocketServer server(PORT, &encoder, &pulseoxy);
     server.start();
+
+    
+
+    
     return 0;
 }
