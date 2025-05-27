47cdb314af5ac60ac2f0edfa53be049578b231d9
LvndnB <150924843+LvndnB@users.noreply.github.com>
2025-05-27 13:21:49 +0200

    Update procflow.cpp

--- procflow.cpp
+++ procflow.cpp
@@ -32,7 +32,7 @@ bool procflow::sendDataToDevice(device_t device, char *data, int lenght) {
 rx_request_response procflow::requestDataFromDevice(device_t device, sensors_and_actuator_enum sensor) {
     uint8_t pdu_part[2] = {'r', device};
     bus.send(pdu_part, 2);
-    //bus.send(key, key_size);
+    bus.send(&sensor, 1);
 
     char *recv = (char *) malloc(200);
     auto rapport = bus.receive_null_termenated(recv, 200);
