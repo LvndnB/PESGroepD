f90e0491dc8a6562e92b888b322d4854f4a910ff
Aron <aron@dingweb.nl>
2025-06-16 21:45:57 +0200

    Document socketclient

--- SocketClient.cpp
+++ SocketClient.cpp
@@ -4,6 +4,9 @@
 #include <cstring>
 #include <iostream>
 
+/**
+ * opens a socket file descriptor
+ */
 SocketClient::SocketClient() {
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
@@ -11,6 +14,9 @@ SocketClient::SocketClient() {
     }
 }
 
+/**
+ * closes a file descriptor
+ */
 SocketClient::~SocketClient() {
     if (sock >= 0) {
         close(sock);
@@ -18,8 +24,10 @@ SocketClient::~SocketClient() {
     }
 }
 
+/**
+ * connect and send the contents in a tcp
+ */
 void SocketClient::send(const std::string& ip, int port, const std::string& message) {
-    usleep(10000);
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
@@ -28,7 +36,4 @@ void SocketClient::send(const std::string& ip, int port, const std::string& mess
     if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == 0) {
         ::send(sock, message.c_str(), message.length(), 0);
     }
-
-    usleep(10000);
-    
 }
