abf01dce3e73f1e7128c0bd755e6e4826d487f38
LvndnB <23095504@student.hhs.nl>
2025-06-16 12:42:59 +0200

    Add doxygen

---
+++ docs/html/search/all_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['socketclient_0',['SocketClient',['../classSocketClient.html',1,'']]],
+  ['socketserver_1',['SocketServer',['../classSocketServer.html',1,'']]],
+  ['stm1_2',['STM1',['../classSTM1.html',1,'']]]
+];
---
+++ docs/html/search/all_4.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['wemos_0',['Wemos',['../classWemos.html',1,'']]],
+  ['wemosdledenbuzzer_1',['WemosDLEDenBuzzer',['../classWemosDLEDenBuzzer.html',1,'']]],
+  ['wemosencoder_2',['WemosEncoder',['../classWemosEncoder.html',1,'']]],
+  ['wemospulseoxy_3',['WemosPulseOxy',['../classWemosPulseOxy.html',1,'']]]
+];
