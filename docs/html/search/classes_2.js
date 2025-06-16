f0ed4c02a67194454b9f5e9a1844f4d79dcf443b
LvndnB <23095504@student.hhs.nl>
2025-06-16 12:48:53 +0200

    Remove wrong classes

--- docs/html/search/classes_2.js
+++ docs/html/search/classes_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['socketclient_0',['SocketClient',['../classSocketClient.html',1,'']]],
-  ['socketserver_1',['SocketServer',['../classSocketServer.html',1,'']]],
-  ['stm1_2',['STM1',['../classSTM1.html',1,'']]]
+  ['socketserver_1',['SocketServer',['../classSocketServer.html',1,'']]]
 ];
