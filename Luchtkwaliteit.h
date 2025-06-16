490b0d18090bc03a69c63f2c38492b94d922bc72
Aron <aron@dingweb.nl>
2025-06-16 21:04:06 +0200

    Document luchtkwaliteit

--- Luchtkwaliteit.h
+++ Luchtkwaliteit.h
@@ -10,7 +10,19 @@ private:
 
 public:
     Luchtkwaliteit(Ventilator* ventilator);
+
+    /**
+     * Deze functie vraagt de temperatuur, luchtvochtigheid en co2 waarde en stuurt
+     * die informatie automatisch naar luchtkwaliteitNaarSpeed gestuurd.
+     * 
+     * Ook stuurt deze functie alle gemeten waardes naar de log 
+     */
     void requestFromSensor();
+
+    /**
+     * Deze functie berekent met de hand van de gemeten waardes de snelheid van de ventilator.
+     * Wanneer ie klaar is met berekenen verstuurd het de snelheid naar  Ventilator*
+     */
     void luchtkwaliteitNaarSpeed();
 
 private:
